fb19ec5fe541ab0262f6f8bde1b280a593473c1e
Sai Sankar Gochhayat <saisankargochhayat@gmail.com>
2018-06-21 17:22:16 +0530

    found min max and avg

--- pinger/app.js
+++ pinger/app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var ping = require('./ping_service')
+var sendNrequest = require('./ping_service')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- pinger/ping_service.js
+++ pinger/ping_service.js
@@ -1,62 +1,34 @@
 var request = require('request');
-var statusCodes = require('http').STATUS_CODES;
 
-function Ping(opts){
-    this.website = 'google.com';
-    this.timeout = 5;
-    this.handle = null;
-    this.init(opts);
-}
-
-Ping.prototype = {
-    init : function(opts){
-        var self = this;
-        self.website = opts.website;
-        self.timeout = opts.timeout;
-        self.start();
-    },
-    start : function() {
-        var self = this;
-        var time = Date.now();
-        console.log('Loading ' + self.website + " Time "+time+'\n');
-        self.handle = setInterval(function(){
-            self.ping();
-        },self.timeout);
-    },
-    isOK : function(time){
-        console.log(time)
-    },
-    isNotOk : function(){
-        console.log("It is Not Okay");
-    },
-    ping : function(){
-        console.log("Trying to ping")
-        var self = this;
-        try {
-            var currentTime = Date.now();
-            request.get({
-                    url : self.website,
-                    time : true
-                },function(error,res,body){
-                if(!error && res.statusCode == 200){
-                    self.isOK(Date.now() - currentTime);
-                }
-                else if(error){
-                    console.log(error)
-                    self.isNotOk();
+function sendNRequests(count,website,cb){
+    var arr = [];
+    var currentTime = Date.now();
+    var i = 0;
+    function sendPing(index){
+        console.log(index)
+        if (index >= count){
+            cb(arr);
+        }else{
+            request.get({url:website},function(error,response){
+                if(!error && response.statusCode == 200){
+                    arr.push(Date.now()-currentTime);
+                    currentTime = Date.now();
+                    sendPing(index+1);
                 }else{
-                    self.isNotOk();
+                    console.log(error)
                 }
-            });
+            })
         }
-        catch(err){
-            console.log(err);
-            self.isNotOk();
-        }
-    },
-    stop : function(){
-        clearInterval(this.handle);
-        this.handle = null;
     }
+    sendPing(i);
 }
+//URL to be pinged
+var website = 'https://dog.ceo/api/breeds/image/random';
+//Number of times to call the api
+var count = 1000;
+//This will be called when all the requests have been completed
+var callback = function(arr){
+    console.log(arr);
+};
 
+module.exports = sendNRequests;
